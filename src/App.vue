9285a92767948e879940332701e440f5d854a751
sunil bandaru <sreddy@foundationmedicine.com>
2018-03-07 21:34:51 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="{'nav-open': $sidebar.showSidebar}">
+    <router-view></router-view>
+    <!--This sidebar appears only for screens smaller than 992px-->
+    <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
+      <ul class="nav navbar-nav">
+        <li>
+          <a class="dropdown-toggle" data-toggle="dropdown">
+            <p>Home</p>
+          </a>
+        </li>
+        <li>
+          <a>
+            <p>Test 1</p>
+          </a>
+        </li>
+        <li>
+          <a>
+            <p>Test 2</p>
+          </a>
+        </li>
+        <li>
+          <a>
+            <p>Test 3</p>
+          </a>
+        </li>
+        <li>
+          <a>
+            <p>Test 4</p>
+          </a>
+        </li>
+        <li class="divider"></li>
+      </ul>
+    </side-bar>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Dashboard/Layout/TopNavbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" :class="{toggled: $sidebar.showSidebar}" @click="toggleSidebar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar bar1"></span>
+          <span class="icon-bar bar2"></span>
+          <span class="icon-bar bar3"></span>
+        </button>
+        <a class="navbar-brand">{{routeName}}</a>
+      </div>
+      <div class="navbar-right-menu">
+        <!--<router-link v-for="(link, index) in sidebarLinks" :to="link.path" tag="li" :ref="link.name">-->
+
+        <ul class="nav navbar-nav navbar-right">
+          <router-link v-for="(link,index) in sidebarLinks" :to="link.path" tag="li" :ref="link.name">
+            <a>
+              <i :class="link.icon"></i>
+
+              <p>{{link.name}}
+              </p>
+            </a>
+          </router-link>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    computed: {
+      routeName () {
+        const {name} = this.$route
+        return this.capitalizeFirstLetter(name)
+      }
+    },
+    data () {
+      return {
+        sidebarLinks: [
+          {
+            name: 'Home',
+            icon: 'ti-panel',
+            path: '/admin/overview'
+          },
+          {
+            name: 'Test 1',
+            icon: 'ti-user',
+            path: '/admin/stats'
+          },
+          {
+            name: 'Test 2',
+            icon: 'ti-view-list-alt',
+            path: '/admin/table-list'
+          },
+          {
+            name: 'Test 3',
+            icon: 'ti-text',
+            path: '/admin/typography'
+          },
+          {
+            name: 'Test 4',
+            icon: 'ti-pencil-alt2',
+            path: '/admin/icons'
+          }
+        ],
+        activeNotifications: false
+      }
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
